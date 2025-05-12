45abf0a4044533836182452bed15c66299345aa6
Lopez Gonzalo <gonzalo@smtv.dev>
2025-05-12 20:06:53 -0300

    test run aptos command

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  test(): string {
+  test() {
     return this.appService.test();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
+import PromisifiedExec from './utils/promisified-exec';
 
 @Injectable()
 export class AppService {
-  test(): string {
-    return 'Hello World!';
+  async test() {
+    const promisifiedExec: PromisifiedExec = new PromisifiedExec();
+    const command = `aptos`;
+
+    const child = promisifiedExec.exec(command, {});
+
+    const res = await child;
+    console.log('res: ', res);
   }
 }
